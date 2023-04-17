cc1a8c1c921e76f6e997cfa6463835e3602eda9f
Alexey888888 <brdkmail@gmail.com>
2023-04-17 17:31:38 +0300

    feat: done binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -26,15 +26,69 @@ class BinarySearchTree {
     this.rootNode = addData(this.rootNode, data);
   }
 
-  has(data) {}
+  has(data) {
+    const hasData = function (node, data) {
+      if (!node) return false;
+      if (node.data === data) return true;
+      if (node.data > data) return hasData(node.left, data);
+      if (node.data < data) return hasData(node.right, data);
+    };
+    return hasData(this.rootNode, data);
+  }
 
-  find(data) {}
+  find(data) {
+    const findData = function (node, data) {
+      if (!node) return null;
+      if (node.data === data) return node;
+      if (node.data > data) {
+        return findData(node.left, data);
+      } else return findData(node.right, data);
+    };
+    return findData(this.rootNode, data);
+  }
 
-  remove(data) {}
+  remove(data) {
+    const removeData = function (node, data) {
+      if (!node) return false;
+      if (node.data > data) {
+        node.left = removeData(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeData(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) return false;
+        if (!node.left) return node.right;
+        if (!node.right) return node.left;
+        let minRight = node.right;
+        while (minRight.left) {
+          minRight = minRight.left;
+        }
+        node.data = minRight.data;
+        node.right = removeData(node.right, minRight.data);
+        return node;
+      }
+    };
+    this.rootNode = removeData(this.rootNode, data);
+  }
 
-  min() {}
+  min() {
+    if (!this.root) return false;
+    let node = this.rootNode;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.data;
+  }
 
-  max() {}
+  max() {
+    if (!this.root) return false;
+    let node = this.rootNode;
+    while (node.right) {
+      node = node.right;
+    }
+    return node.data;
+  }
 }
 
 module.exports = {
